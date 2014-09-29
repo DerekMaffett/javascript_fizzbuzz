a155a65f987f32f8c67b7ac6e12fe837eef53d91
Derek Maffett <derekgmaffett@gmail.com>
2014-09-29 12:38:04 -0700

    Add initial fizzbuzz implementation

---
+++ fizzbuzz.js
@@ -0,0 +1,28 @@
+function fizzbuzzLoop(start, end) {
+  for (i = start; i == end; i++) {
+    fizzbuzz(num)
+  }
+}
+
+function fizzbuzz(num) {
+  if !(fizz(num) || buzz(num)) {
+    return console.log(num)
+  } else {
+    result = ''
+    if (fizz(num)) {
+      result + 'fizz'
+    }
+    if (buzz(num)) {
+      result + 'buzz'
+    }
+  }
+  console.log(result)
+}
+
+function fizz(num) {
+  return num % 3 == 0
+}
+
+function buzz(num) {
+  return num % 5 == 0
+}
